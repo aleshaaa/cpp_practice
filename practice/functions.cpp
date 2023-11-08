86f7f2d52d5635013f9907fac533a091145b0e65
aleshaaa <55158624+aleshaaa@users.noreply.github.com>
2023-11-08 16:39:47 +0100

    added code in method biggestDiff

--- practice/functions.cpp
+++ practice/functions.cpp
@@ -2,5 +2,15 @@
 
 int functions::biggestDiff(std::vector<int> arr)
 {
-    return 0;
+    int max = arr[0];
+    int min = arr[0];
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+    return max - min;
 }
